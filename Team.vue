31ca2457696fca6e0d3b90fe53249d8bf149fbbd
Ana Silvia Cruz <85088477+aigleetlion@users.noreply.github.com>
2021-10-13 13:14:44 -0500

    Se agrega la vista Team
    Se adiciona Team.vue y CardTeam.vue, también las imágenes relacionadas, el json de integrantes se envía por mensaje privado

---
+++ Team.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h1>Equipo de desarrollo</h1>
+        <div class="row">
+            <CardTeam></CardTeam>
+        </div> 
+    </div>
+</template>
+<script>
+import CardTeam from "../components/CardTeam.vue";
+
+    export default {
+    name: "App",
+    components: {
+        CardTeam,
+    },
+    };
+</script>
